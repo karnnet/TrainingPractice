af55a9b6d554f71bda15e9efc120d8b047d951de
navneet <knavneet@qualminds.com>
2022-06-09 18:14:23 +0530

    elseif condition

---
+++ Javascript/elseif.js
@@ -0,0 +1,19 @@
+var a = 30;
+var b = 30;
+if (a < b) {
+    console.log("b is greater than a");
+} else if (a > b) {
+    console.log("a is greater than a");
+} else {
+    console.log("a is equal to b");
+}
+
+
+const time = new Date().getHours();
+if (time < 4) {
+    console.log("good afternoon");
+} else if (time > 6) {
+    console.log("good evening")
+} else {
+    console.log("good night");
+}
