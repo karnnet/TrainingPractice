0839a001f6d910734ea70a2e5f33337544ee129a
navneet <knavneet@qualminds.com>
2022-06-30 21:08:00 +0530

    ajax call

---
+++ jQuery/ajax.js
@@ -0,0 +1,28 @@
+// $(() => {
+//     $.ajax({
+//         type: 'GET',
+//         url: 'https://jsonplaceholder.typicode.com/posts',
+//         success: function(data) {
+//             console.log(data);
+//         },
+//         error: function() {
+//             console.log('error');
+//         },
+//         complete: function() {
+//             console.log('complete');
+//         }
+//     });
+
+// });
+
+
+
+$(() => {
+    $.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
+        console.log(data);
+        $.get('https://jsonplaceholder.typicode.com/posts/' + data[2].id).then(function(response) {
+            console.log(response);
+        });
+    });
+
+});
