b8355b43c066e3351128c4e9c82f876099b232bb
navneet <knavneet@qualminds.com>
2022-07-12 12:04:13 +0530

    Intial commit and write first Typescript program

---
+++ Typescript/main.js
@@ -0,0 +1,5 @@
+function addnumbers(a, b) {
+    return a * b;
+}
+var result = addnumbers(1, 2);
+console.log(result);
---
+++ Typescript/main.ts
@@ -0,0 +1,5 @@
+ function addnumbers(a:number,b:number){
+     return a*b;
+ }
+ const result=addnumbers(1,2);
+ console.log(result);
