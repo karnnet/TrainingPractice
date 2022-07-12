d49e71f00b9541786e1686caa68edbec4c75277a
navneet <knavneet@qualminds.com>
2022-07-12 12:51:12 +0530

    Use any data type

--- Typescript/main.js
+++ Typescript/main.js
@@ -1,5 +1,10 @@
 function addnumbers(a, b) {
-    return a * b;
+    return a + b;
 }
 var result = addnumbers(1, 2);
 console.log(result);
+function subtractnumbers(a, b) {
+    return a - b;
+}
+var results = subtractnumbers(1, 2);
+console.log(results);
--- Typescript/main.ts
+++ Typescript/main.ts
@@ -1,5 +1,11 @@
- function addnumbers(a:number,b:number){
-     return a*b;
+ function addnumbers(a:any,b:any){// by using any type, it will accept any data types
+     return a+b;
  }
  const result=addnumbers(1,2);
  console.log(result);
+
+ function subtractnumbers(a,b){
+    return a-b;
+}
+const results=subtractnumbers(1,2);
+console.log(results);
