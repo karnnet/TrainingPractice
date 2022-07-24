dbe69449c00fa7f5bd25854416d7cf567fcc8df4
navneet <knavneet@qualminds.com>
2022-07-24 23:06:36 +0530

    Practice Typescript

---
+++ Typescript/Interface/interface.js
@@ -0,0 +1,5 @@
+var user = {
+    name: 'abc',
+    age: 30
+};
+console.log(user);
---
+++ Typescript/Interface/interface.ts
@@ -0,0 +1,10 @@
+interface userType{
+    name:string,
+    age:number
+}
+
+let user:userType={
+    name:'abc',
+    age:30
+}
+console.log(user);
